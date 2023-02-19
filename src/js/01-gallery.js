52b1fb3b3ac6057c7d4aadd9bf13301a2ac38004
Artem <artembariga5@gmail.com>
2023-02-19 19:54:51 +0200

    done task one

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Галерея 
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+
+const listEl = document.querySelector('.gallery');
+const dataGallery = galleryItems.map(renderGallery).join('');
+
+
+function renderGallery ({original, preview, description}) {
+    return`
+    <li class=''gallery__item>
+        <a class="gallery__link" href="${original}" >
+            <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+        </a>
+    </li>
+    `;
+}
+
+listEl.insertAdjacentHTML('beforeend', dataGallery);
+
+new SimpleLightbox('.gallery a', { captionDelay:250});
+
+
