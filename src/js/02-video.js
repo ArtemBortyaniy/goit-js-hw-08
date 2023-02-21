21b8f63d0b6f2951b1cf9ed94947a4abf4216e8c
Artem <artembariga5@gmail.com>
2023-02-21 14:57:34 +0200

    done all tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+
+const videoContainer = document.querySelector('#vimeo-player');
+const LOCALSTORAGE_KEY  = "videoplayer-current-time";
+
+const player = new Player(videoContainer);  
+
+const jsoncurrentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+const currentTime = JSON.parse(jsoncurrentTime);
+
+player.on('timeupdate', throttle(saveTimeVideo, 1000));
+
+function saveTimeVideo(e) {
+    localStorage.setItem(LOCALSTORAGE_KEY, e.seconds);
+}
+
+player.setCurrentTime(currentTime).then(function() {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+var throttle = require('lodash.throttle');
+
+const formEl = document.querySelector('.feedback-form');
+const inputEmailEl = document.querySelector('[name="email"]');
+const inputMessageEl = document.querySelector('[name="message"]');
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+updateForm();
+
+formEl.addEventListener('input', throttle(saveInLocalStorage, 500));
+formEl.addEventListener('submit', resetForm);
+
+function saveInLocalStorage () {
+    const email = formEl.elements.email.value;
+    const message = formEl.elements.message.value;
+    
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify({
+        email : email, 
+        message : message,
+    })); 
+}
+
+function updateForm () {
+    const jsonValueForm = localStorage.getItem(LOCALSTORAGE_KEY);
+    const localStorageObj = JSON.parse(jsonValueForm);
+
+    if (localStorageObj) {
+        inputEmailEl.value = localStorageObj.email || '';
+        inputMessageEl.value = localStorageObj.message || '';
+    }
+}
+
+function resetForm (e) {
+    e.preventDefault();
+    
+    const jsonValueForm = localStorage.getItem(LOCALSTORAGE_KEY);
+    const localStorageObj = JSON.parse(jsonValueForm);
+    console.log(localStorageObj);
+
+    localStorage.clear();
+    formEl.reset();
+}
 
