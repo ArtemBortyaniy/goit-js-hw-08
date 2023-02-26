c1d8b4cb74f1484a05cb74f02a78b2c820a56628
Artem <artembariga5@gmail.com>
2023-02-26 14:10:18 +0200

    optimized code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -12,7 +12,7 @@ const dataGallery = galleryItems.map(renderGallery).join('');
 
 function renderGallery ({original, preview, description}) {
     return`
-    <li class=''gallery__item>
+    <li class="gallery__item">
         <a class="gallery__link" href="${original}" >
             <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
         </a>
